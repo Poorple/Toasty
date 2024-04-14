01939f7a19a3202440191a8174f69e77966a734f
Poorple <91628099+Poorple@users.noreply.github.com>
2024-04-14 22:03:49 +0200

    responsiveness update

--- src/components/OurBreads.tsx
+++ src/components/OurBreads.tsx
@@ -1,18 +1,49 @@
-import { useEffect } from "react";
+import { useEffect, useRef, useState } from "react";
 import breads from "../local-json/bread.json";
 import "../styles/our-breads.scss";
 
 const OurBreads = () => {
   let breadsObj = breads.breads;
+
+  const [isVisible, setIsVisible] = useState(false);
+  const [animationPlayed, setAnimationPlayed] = useState(false);
+  const ref = useRef(null);
+
   useEffect(() => {
-    console.log(breadsObj);
-  }, []);
+    const observer = new IntersectionObserver(
+      ([entry]) => {
+        if (entry.isIntersecting && !animationPlayed) {
+          setIsVisible(true);
+          setAnimationPlayed(true);
+        }
+      },
+      {
+        root: null,
+        rootMargin: "0px",
+        threshold: 0.3,
+      }
+    );
+
+    if (ref.current) {
+      observer.observe(ref.current);
+    }
+
+    return () => {
+      if (ref.current) {
+        observer.unobserve(ref.current);
+      }
+    };
+  }, [animationPlayed]);
+
   return (
-    <div className="bread-div">
+    <div ref={ref} className="bread-div">
       <h2>Our Breads</h2>
       <section className="bread-sect">
         {breadsObj.map((x) => (
-          <article key={x.breadName}>
+          <article
+            className={isVisible ? "visible" : "hidden"}
+            key={x.breadName}
+          >
             <img src={x.srcPath} />
             <div className="desc-div">
               <h3>{x.breadName}</h3>
