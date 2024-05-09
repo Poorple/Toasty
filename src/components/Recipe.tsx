f40db485d763f5e1983d77fb7830389778a83878
Poorple <91628099+Poorple@users.noreply.github.com>
2024-05-09 18:04:04 +0200

    uniform height addition

--- src/components/Recipe.tsx
+++ src/components/Recipe.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect, useRef, useState } from "react";
 import { useParams } from "react-router-dom";
 import breads from "../local-json/bread.json";
 import "../styles/recipe.scss";
@@ -15,8 +15,24 @@ const Recipe = () => {
   const breadObj = breads.breads;
   const [formattedUrlParam, setFormattedUrlParam] = useState<string>("");
   const [selectedBread, setSelectedBread] = useState<DynamicObject>();
+  const [stepHeight, setStepHeight] = useState<number>();
 
   const { bread } = useParams<{ bread: string }>();
+
+  const firstElementRef = useRef<HTMLDivElement>(null);
+  const secondElementRef = useRef<HTMLDivElement>(null);
+
+  useEffect(() => {
+    if (firstElementRef.current && secondElementRef.current) {
+      const secondElementHeight = stepHeight;
+      firstElementRef.current.style.height = `${secondElementHeight}px`;
+    }
+  }, [stepHeight]);
+
+  useEffect(() => {
+    if (secondElementRef.current?.clientHeight)
+      setStepHeight(secondElementRef.current.clientHeight);
+  }, [secondElementRef.current?.clientHeight]);
 
   useEffect(() => {
     if (bread) {
@@ -68,25 +84,27 @@ const Recipe = () => {
       <section className="header-and-img-sect">
         <img src={selectedBread?.srcPath} />
       </section>
-      <section className="aa">
+      <section className="ingredients-and-step-sect">
         {selectedBread ? (
-          <div className="prep-and-ingredients-sect">
-            <h2>{selectedBread?.breadName}</h2>
-            <div>
-              <h3>Prep Time</h3>
-              {Object.keys(selectedBread.prepTime[0]).map((x) => (
-                <p>{`${capitalizeFirstLetter(x)}: ${
-                  selectedBread.prepTime[0][x]
-                }`}</p>
-              ))}
-            </div>
-            <div className="ingredients-div">
-              <h3>Ingredients</h3>
-              {Recurs(selectedBread.ingredients)}
+          <div className="prep-and-ingredients-sect" ref={firstElementRef}>
+            <div style={{ position: "sticky", top: 0 }}>
+              <h2>{selectedBread?.breadName}</h2>
+              <div>
+                <h3>Prep Time</h3>
+                {Object.keys(selectedBread.prepTime[0]).map((x) => (
+                  <p>{`${capitalizeFirstLetter(x)}: ${
+                    selectedBread.prepTime[0][x]
+                  }`}</p>
+                ))}
+              </div>
+              <div className="ingredients-div">
+                <h3>Ingredients</h3>
+                {Recurs(selectedBread.ingredients)}
+              </div>
             </div>
           </div>
         ) : null}
-        <div className="step-sect">
+        <div className="step-sect" ref={secondElementRef}>
           <h3>Steps</h3>
           {selectedBread ? (
             <ol>
